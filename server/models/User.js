203ce0951ba93c6a0711234e9a1444a3c85d845f
anelson92 <amanda92nelson@gmail.com>
2023-04-06 18:58:36 -0500

    typeDefs

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,19 @@
 // username, password, email, phone number (for reminder texts, maybe)
+const { Schema, model} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true, 
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String, 
+            required: true, 
+            unique: true, 
+            format: email 
+        }
+    }
+)
